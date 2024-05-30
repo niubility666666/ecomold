1a10cb7f6a3048a1f6dc1c9a3f1c106ed39b332f
yukun <1466958057.@qq.com>
2024-05-30 20:25:31 +0800

    提交初始化版本

---
+++ ecomold-front/src/main/java/com/ecomold/EcoMoldApplication.java
@@ -0,0 +1,15 @@
+package com.ecomold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yukun
+ */
+@SpringBootApplication
+public class EcoMoldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcoMoldApplication.class);
+        System.out.println("EcoMoldApplication start!");
+    }
+}
